4357427a6d79d2e0fea83748f4bc562fca851008
Ryan Zimmerman <zimmerry@oregonstate.edu>
2021-01-21 12:43:29 -0800

    Add handlebars rendering and adding/removing monitored websites

---
+++ public/index.js
@@ -0,0 +1,97 @@
+function insertNewSite(url) {
+    var context = {
+        url: url
+    };
+
+    var siteHTML = Handlebars.templates.site(context);
+
+    websiteSection = document.getElementById('websites');
+    websiteSection.insertAdjacentHTML('beforeEnd', siteHTML);
+
+    var websiteCheckBoxes = document.getElementsByClassName('website-check');
+    for(var i = 0; i < websiteCheckBoxes.length; i++) {
+        websiteCheckBoxes[i].addEventListener('click', removeSite);
+    }
+}
+
+
+var allSites = [];
+
+function handleNewWebsiteButtonClick() {
+
+    var url = document.getElementById('new-website-text').value.trim();
+
+    if (!url) {
+        alert("You must enter a URL!");
+    } else {
+        var newSiteReq = new XMLHttpRequest();
+        newSiteReq.open('POST', '/addSite');
+
+        var reqBody = JSON.stringify({
+            url: url
+        });
+
+        newSiteReq.setRequestHeader('Content-type', 'application/json');
+        newSiteReq.addEventListener('load', function(event) {
+            if (event.target.status === 200) {
+                insertNewSite(url);
+            } else {
+                alert("Error storing website in database: " + event.target.response);
+            }
+        });
+
+        newSiteReq.send(reqBody);
+
+        document.getElementById('new-website-text').value = "";
+
+  }
+
+}
+
+function removeSite() {
+    var websites = document.getElementsByClassName('website-check');
+
+    for(var i = 0; i < websites.length; i++) {
+        if(websites[i].checked) {
+            var urlToRemove = websites[i].parentNode.getAttribute("data-url")
+            websites[i].parentNode.remove();
+        }
+    }
+
+    var removeRequest = new XMLHttpRequest();
+    removeRequest.open('POST', '/removeSite');
+
+    var reqBody = JSON.stringify({
+        urlToRemove: urlToRemove
+    });
+
+    removeRequest.setRequestHeader('Content-type', 'application/json');
+
+    removeRequest.addEventListener('load', function (event) {
+        if (event.target.status === 200) {
+
+        } else {
+            alert("Error removing website from database: " + event.target.response);
+        }
+    });
+
+    removeRequest.send(reqBody);
+}
+
+
+/*
+ * Wait until the DOM content is loaded, and then hook up UI interactions, etc.
+ */
+window.addEventListener('DOMContentLoaded', function () {
+
+    var newWebsiteButton = document.getElementById('new-website-button');
+    if(newWebsiteButton) {
+        newWebsiteButton.addEventListener('click', handleNewWebsiteButtonClick)
+    }
+
+    var websiteCheckBoxes = document.getElementsByClassName('website-check');
+    for(var i = 0; i < websiteCheckBoxes.length; i++) {
+        websiteCheckBoxes[i].addEventListener('click', removeSite);
+    }
+
+});
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 const axios = require('axios');
 const http = require("http");
 const https = require('https');
@@ -13,9 +14,8 @@ const port = 8686
 
 require('console-stamp')(console, 'HH:MM:ss.l');
 
-cron.schedule('*/10 * * * *', () => {
-    test()
-})
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -23,8 +23,55 @@ app.use(express.static('public'));
 app.get('/', function (req, res, next) {
     console.log("== received GET request for /");
 
-    res.status(200).sendFile(__dirname, 'index.html')
+    res.status(200).render('sitePage', {site: websites})
 })
+
+app.post('/addSite', function(req, res, next) {
+    if(req.body && req.body.url) {
+        websites.push({
+            url: req.body.url
+        });
+
+        fs.writeFile(
+            __dirname + '/sites.json',
+            JSON.stringify(websites, null, 2),
+            function(err, data) {
+                if (err) {
+                    console.log(" == err: ", err);
+                    res.status(500).send("Error saving website to DB");
+                } else {
+                    res.status(200).send("Website successfully added.");
+                }
+            }
+        )
+    } else {
+        res.status(400).send("Request body must contain 'url'.");
+    }
+});
+
+app.post('/removeSite', function(req, res, next) {
+    if(req.body && req.body.urlToRemove) {
+        for(site in websites) {
+            if(websites[site]["url"] == req.body.urlToRemove) {
+                websites.splice(site, 1);
+            }
+        }
+        fs.writeFile(
+            __dirname + '/sites.json',
+            JSON.stringify(websites, null, 2),
+            function(err, data) {
+                if(err) {
+                    console.log(" == err: ", err);
+                    res.status(500).send("Error removing website from DB");
+                } else {
+                    res.status(200).send("Website successfully removed.");
+                }
+            }
+        )
+    } else {
+        res.status(400).send("Request body must contain 'urlToRemove'.");
+    }
+});
 
 function sendTextMessage(url) {
     axios.post(config["url"], {
@@ -54,9 +101,13 @@ function checkWebsite(url) {
 
 function test(){
     websites.forEach(function(item, index, array) {
-        var check = checkWebsite(item["url"]);
+        checkWebsite(item["url"]);
     })
 }
+
+cron.schedule('*/10 * * * *', () => {
+    test()
+})
 
 app.listen(port, function () {
     console.log('Downtime Alerter listening on port 8686')
